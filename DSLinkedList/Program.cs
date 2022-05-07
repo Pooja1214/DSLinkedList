76ce91a61085f5c407fe56162662357a3e4d5227
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 17:48:46 +0530

    FindNode

---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureProblems
+{/// <summary>
+/// Template for testing link list
+/// </summary>
+    class Program
+    {/// <summary>
+     /// UC6 - Delete Last element of linked list Using pop pos
+     /// </summary>
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.Add(56);
+            myList.Add(30);
+            myList.Add(70);
+
+            Console.WriteLine(myList.Search(30));
+            
+
+        }
+    }
+}
