f3f762e85e5bcccd56484fd067e3d980e654039c
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 17:36:03 +0530

    LinkedListBYAppending

---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureProblems
+{/// <summary>
+/// Template for testing link list
+/// </summary>
+    class Program
+    {/// <summary>
+     /// UC3 - Inserting elements by appending in  linked list
+     /// </summary>
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.Add(56);
+            myList.Add(30);
+            myList.Add(70);
+
+            myList.Display();
+        }
+    }
+}
