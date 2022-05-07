1ae11f9542599879d19423458d9ea6b2448de6bd
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 17:28:36 +0530

    InsetingElementAtFront

---
+++ DSLinkedList/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DataStructureProblems
+{
+    internal class LinkedList<Gtype>
+    {
+        Node<Gtype> head = null;
+
+        public void Add(Gtype data)
+        {
+            //variable
+            //store the count in the temp_node 
+            Node<Gtype> temp_node;
+
+            //creating new node with data
+            Node<Gtype> node = new Node<Gtype>(data);
+
+            //there is no node 
+            if (head == null)
+            {
+                //if there is no element so first one is head
+                head = node;
+                //close the executaion of the function
+                return;
+            }
+            else
+            {
+                //count start from head
+                temp_node = head;
+                //checking for null
+                while (temp_node.next != null)
+                {
+                    temp_node = temp_node.next;
+                }
+                //adding the node to next variable
+                temp_node.next = node;
+            }
+        }
+    }  
+}
---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureProblems
+{/// <summary>
+/// Template for testing link list
+/// </summary>
+    class Program
+    {/// <summary>
+     /// UC2 - Inserting elements at front in linked list
+     /// </summary>
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.Add(70);
+            myList.Add(30);
+            myList.Add(56);
+
+        }
+    }
+}
