820f36092690b90910951e2266789772f618155b
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-07 17:54:30 +0530

    DeleteMiddleElement

---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructureProblems
+{/// <summary>
+/// Template for testing link list
+/// </summary>
+    class Program
+    {/// <summary>
+     /// UC9 - Deleting element in between in LinkedList 
+     /// </summary>
+
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            myList.Add(56);
+            myList.Add(30);
+            myList.Add(70);
+            Console.WriteLine("Before position adding");
+            myList.Display();
+            myList.Add(40, 3);
+            Console.WriteLine("After position adding");
+            myList.Display();
+            Console.WriteLine("After removing");
+            myList.Remove(3);
+            myList.Display();
+        }
+    }
+}
